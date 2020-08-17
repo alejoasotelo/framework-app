18b61e050336bd68aeb95a00d9f18810dda2b643
Alejo Sotelo <alejoasotelo@outlook.com>
2020-08-17 16:08:47 -0300

    [*] Fix install script.

--- App/Setup/InstallScript.php
+++ App/Setup/InstallScript.php
@@ -9,51 +9,38 @@ namespace App\Setup;
 use Composer\Script\Event;
 
 /**
- * Post-install class triggered during 'compser install' to set up system dependencies
+ * Post-install class triggered during 'compser install' to set up system dependencies.
  *
  * @since  1.0
  */
 class InstallScript
 {
     public static function postInstall(Event $event)
-	{
-		// Check if a config.json file exists, copy the config.dist.json if not
-		if (!file_exists('App/Config/config.json'))
-		{
-			copy('App/Config/config.dist.json', 'App/Config/config.json');
-		}
+    {
+        // Check if a config.json file exists, copy the config.dist.json if not
+        if (!file_exists('App/Config/config.json')) {
+            copy('App/Config/config.dist.json', 'App/Config/config.json');
+        }
 
-		// Make sure the assets directory exists
-		if (!is_dir('www/assets'))
-		{
-			mkdir('www/assets', 0755);
-		}
+        // Make sure the assets directory exists
+        if (!is_dir('www/assets')) {
+            mkdir('www/assets', 0755);
+        }
 
-		// Symlink img
-		$bsImagePath = '../../vendor/twbs/bootstrap/docs/assets/img';
-		$imgAssetsPath = 'www/assets/img';
+        // Symlink js
+        $bsJSPath = '../../vendor/twbs/bootstrap/dist/js';
+        $jsAssetsPath = 'www/assets/js';
 
-		if (!file_exists($imgAssetsPath))
-		{
-			symlink($bsImagePath, $imgAssetsPath);
-		}
+        if (!file_exists($jsAssetsPath)) {
+            symlink($bsJSPath, $jsAssetsPath);
+        }
 
-		// Symlink js
-		$bsJSPath = '../../vendor/twbs/bootstrap/docs/assets/js';
-		$jsAssetsPath = 'www/assets/js';
+        // Symlink css
+        $bsCSSPath = '../../vendor/twbs/bootstrap/dist/css';
+        $cssAssetsPath = 'www/assets/css';
 
-		if (!file_exists($jsAssetsPath))
-		{
-			symlink($bsJSPath, $jsAssetsPath);
-		}
-
-		// Symlink css
-		$bsCSSPath = '../../vendor/twbs/bootstrap/docs/assets/css';
-		$cssAssetsPath = 'www/assets/css';
-
-		if (!file_exists($cssAssetsPath))
-		{
-			symlink($bsCSSPath, $cssAssetsPath);
-		}
-	}
+        if (!file_exists($cssAssetsPath)) {
+            symlink($bsCSSPath, $cssAssetsPath);
+        }
+    }
 }
