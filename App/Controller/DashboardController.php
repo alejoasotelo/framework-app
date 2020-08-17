c5f3574137bcb12d28eb673e7af9cf4f5107b9d9
Alejo Sotelo <alejoasotelo@outlook.com>
2020-08-17 16:28:05 -0300

    [*] refactor

--- App/Controller/DashboardController.php
+++ App/Controller/DashboardController.php
@@ -6,39 +6,34 @@
 
 namespace App\Controller;
 
-use Joomla\Application\AbstractApplication;
-use Joomla\Input\Input;
-use Joomla\Log\Log;
 use App\Model\DashboardModel;
 
 /**
- * Dashboard Controller class for the Application
+ * Dashboard Controller class for the Application.
  *
  * @since  1.0
  */
 class DashboardController extends DefaultController
 {
-	/**
-	 * Task to update the database configuration and install sample data
-	 *
-	 * @return  void
-	 *
-	 * @since   1.0
-	 * @throws  \RuntimeException
-	 */
-	public function updateDatabase()
-	{
-		try
-		{
-			$dashboardModel = new DashboardModel($this->getInput(), $this->getContainer()->get('db'));
-			$dashboardModel->updateDatabase($this->getContainer()->get('config'));
+    /**
+     * Task to update the database configuration and install sample data.
+     *
+     * @return void
+     *
+     * @since   1.0
+     *
+     * @throws \RuntimeException
+     */
+    public function updateDatabase()
+    {
+        try {
+            $dashboardModel = new DashboardModel($this->getInput(), $this->getContainer()->get('db'));
+            $dashboardModel->updateDatabase($this->getContainer()->get('config'));
 
-			$this->getInput()->set('success', true);
-			$this->getApplication()->redirect($this->getApplication()->get('uri.base.path'));
-		}
-		catch (\Exception $e)
-		{
-			throw new \RuntimeException(sprintf('Error: ' . $e->getMessage()));
-		}
-	}
+            $this->getInput()->set('success', true);
+            $this->getApplication()->redirect($this->getApplication()->get('uri.base.path'));
+        } catch (\Exception $e) {
+            throw new \RuntimeException(sprintf('Error: '.$e->getMessage()));
+        }
+    }
 }
--- App/Router/AppRouter.php
+++ App/Router/AppRouter.php
@@ -13,65 +13,65 @@ use Joomla\Input\Input;
 use Joomla\Router\Router;
 
 /**
- * Sample Router
+ * Sample Router.
  *
  * @since  1.0
  */
 class AppRouter extends Router
 {
-	/**
-	 * Application object to inject into controllers
-	 *
-	 * @var    AbstractApplication
-	 * @since  1.0
-	 */
-	protected $app;
+    /**
+     * Application object to inject into controllers.
+     *
+     * @var AbstractApplication
+     *
+     * @since  1.0
+     */
+    protected $app;
 
-	/**
-	 * Constructor.
-	 *
-	 * @param   Input                $input  An optional input object from which to derive the route.  If none
-	 *                                       is given than the input from the application object will be used.
-	 * @param   AbstractApplication  $app    An optional application object to inject to controllers
-	 *
-	 * @since   1.0
-	 */
-	public function __construct(Input $input = null, AbstractApplication $app = null)
-	{
-		parent::__construct($app->input);
+    /**
+     * Constructor.
+     *
+     * @param Input               $input An optional input object from which to derive the route.  If none
+     *                                   is given than the input from the application object will be used.
+     * @param AbstractApplication $app   An optional application object to inject to controllers
+     *
+     * @since   1.0
+     */
+    public function __construct(Input $input = null, AbstractApplication $app = null)
+    {
+        parent::__construct($app->input);
 
-		$this->app = $app;
-	}
+        $this->app = $app;
+    }
 
-	/**
-	 * Get a ControllerInterface object for a given name.
-	 *
-	 * @param   string  $name  The controller name (excluding prefix) for which to fetch and instance.
-	 *
-	 * @return  ControllerInterface
-	 *
-	 * @since   1.0
-	 * @throws  \RuntimeException
-	 */
-	protected function fetchController($name)
-	{
-		// Derive the controller class name.
-		$class = $this->controllerPrefix . ucfirst($name);
+    /**
+     * Get a ControllerInterface object for a given name.
+     *
+     * @param string $name the controller name (excluding prefix) for which to fetch and instance
+     *
+     * @return ControllerInterface
+     *
+     * @since   1.0
+     *
+     * @throws \RuntimeException
+     */
+    protected function fetchController($name)
+    {
+        // Derive the controller class name.
+        $class = $this->controllerPrefix.ucfirst($name);
 
-		// If the controller class does not exist panic.
-		if (!class_exists($class) || !is_subclass_of($class, 'Joomla\\Controller\\ControllerInterface'))
-		{
-			throw new \RuntimeException(sprintf('Unable to locate controller `%s`.', $class), 404);
-		}
+        // If the controller class does not exist panic.
+        if (!class_exists($class) || !is_subclass_of($class, 'Joomla\\Controller\\ControllerInterface')) {
+            throw new \RuntimeException(sprintf('Unable to locate controller `%s`.', $class), 404);
+        }
 
-		// Instantiate the controller.
-		$controller = new $class($this->input, $this->app);
+        // Instantiate the controller.
+        $controller = new $class($this->input, $this->app);
 
-		if ($controller instanceof ContainerAwareInterface)
-		{
-			$controller->setContainer($this->app->getContainer());
-		}
+        if ($controller instanceof ContainerAwareInterface) {
+            $controller->setContainer($this->app->getContainer());
+        }
 
-		return $controller;
-	}
+        return $controller;
+    }
 }
